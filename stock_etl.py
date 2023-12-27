230412b866aa2d7c7bac6ec4b6e96f014f8ed670
pavi-y <154924210+pavi-y@users.noreply.github.com>
2023-12-27 22:49:14 +0000

    Add azure blob logic

--- stock_etl.py
+++ stock_etl.py
@@ -1,17 +1,43 @@
+import os
 import pandas as pd
 import yfinance as yf
+from azure.storage.blob import BlobServiceClient
+from dotenv import load_dotenv
 
-# Define the stock symbols and date range for data extraction
+# Load environment variables from the .env file
+load_dotenv()
+
+# Define the stock symbols, date range, and CSV file path
 symbols = ['AAPL', 'TSLA']
 start_date = '2023-01-01'
 end_date = '2023-12-31'
+csv_file_path = 'stock_data.csv'
 
-# Extract data from Yahoo Finance
-data = pd.DataFrame()
-for symbol in symbols:
-    ticker = yf.Ticker(symbol)
-    stock_data = ticker.history(start=start_date, end=end_date)
-    stock_data['symbol'] = symbol
-    data = pd.concat([data, stock_data])
+# Retrieve Azure parameters from the environment variables
+account_name = os.getenv('AZURE_ACCOUNT_NAME')
+account_key = os.getenv('AZURE_ACCOUNT_KEY')
+container_name = os.getenv('AZURE_CONTAINER_NAME')
+blob_name = 'stock_data.csv'
 
-    print(data)
+# Function to upload a file to Azure Blob Storage
+def save_to_azure_blob(csv_file_path, account_name, account_key, container_name, blob_name):
+    blob_service_client = BlobServiceClient(account_url=f"https://{account_name}.blob.core.windows.net", credential=account_key)
+    container_client = blob_service_client.get_container_client(container_name)
+    blob_client = container_client.get_blob_client(blob_name)
+
+    with open(csv_file_path, "rb") as data:
+        blob_client.upload_blob(data, overwrite=True)
+    print(f"File {csv_file_path} uploaded to Azure Storage container {container_name} as blob {blob_name}")
+
+# Function to extract stock data and save to a CSV file
+def get_stock_data_and_save(symbols, start_date, end_date, csv_file_path):
+    data = pd.DataFrame()
+    for symbol in symbols:
+        ticker = yf.Ticker(symbol)
+        stock_data = ticker.history(start=start_date, end=end_date)
+        stock_data['symbol'] = symbol
+        data = pd.concat([data, stock_data])
+    data.to_csv(csv_file_path)
+    print(f"Stock data saved to {csv_file_path}")
+    save_to_azure_blob(csv_file_path, account_name, account_key, container_name, blob_name)
+
