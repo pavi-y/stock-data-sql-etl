6e6af395ac1a98aa6507dc98be28262cc72ffe1e
pavi-y <154924210+pavi-y@users.noreply.github.com>
2023-12-27 23:13:38 +0000

    Call get stock data function

--- stock_etl.py
+++ stock_etl.py
@@ -4,21 +4,6 @@ import yfinance as yf
 from azure.storage.blob import BlobServiceClient
 from dotenv import load_dotenv
 
-# Load environment variables from the .env file
-load_dotenv()
-
-# Define the stock symbols, date range, and CSV file path
-symbols = ['AAPL', 'TSLA']
-start_date = '2023-01-01'
-end_date = '2023-12-31'
-csv_file_path = 'stock_data.csv'
-
-# Retrieve Azure parameters from the environment variables
-account_name = os.getenv('AZURE_ACCOUNT_NAME')
-account_key = os.getenv('AZURE_ACCOUNT_KEY')
-container_name = os.getenv('AZURE_CONTAINER_NAME')
-blob_name = 'stock_data.csv'
-
 # Function to upload a file to Azure Blob Storage
 def save_to_azure_blob(csv_file_path, account_name, account_key, container_name, blob_name):
     blob_service_client = BlobServiceClient(account_url=f"https://{account_name}.blob.core.windows.net", credential=account_key)
@@ -41,3 +26,20 @@ def get_stock_data_and_save(symbols, start_date, end_date, csv_file_path):
     print(f"Stock data saved to {csv_file_path}")
     save_to_azure_blob(csv_file_path, account_name, account_key, container_name, blob_name)
 
+# Load environment variables from the .env file
+load_dotenv()
+
+# Define the stock symbols, date range, and CSV file path
+symbols = ['AAPL', 'TSLA']
+start_date = '2023-01-01'
+end_date = '2023-12-31'
+csv_file_path = 'stock_data.csv'
+
+# Retrieve Azure parameters from the environment variables
+account_name = os.getenv('AZURE_ACCOUNT_NAME')
+account_key = os.getenv('AZURE_ACCOUNT_KEY')
+container_name = os.getenv('AZURE_CONTAINER_NAME')
+blob_name = 'stock_data.csv'
+
+# Call the function to extract stock data and save to a CSV file
+get_stock_data_and_save(symbols, start_date, end_date, csv_file_path)
